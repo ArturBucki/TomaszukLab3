8900a42c75500307e8b43e01f0aa762b9ad68af2
Artur Bucki <abucki100@gmail.com>
2023-10-30 14:32:15 +0100

    Zaktualizowano aplikację PWA.

--- app.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-        .then(function(registration) {
-            console.log('Service Worker zarejestrowany z sukcesem:', registration);
-        })
-        .catch(function(error) {
-            console.log('Błąd podczas rejestracji Service Workera:', error);
-        });
-}
